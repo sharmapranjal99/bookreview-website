6d50f0a2b32ce8ac8b36bc025b3320c392fa8734
sharmapranjal99 <pranjal.2428cseai762@kiet.edu>
2024-12-17 21:53:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Select the heading element
+const heading = document.getElementById("heading");
+
+// Add an event listener for the click event
+heading.addEventListener("click", function () {
+    alert("This is a standard JavaScript alert pop-up!");
+});
+
